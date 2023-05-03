49980779a8b1a1e1dc0bfb35945d7e188afe82e1
Paolo Di Martino <paolo.dimartino@outlook.it>
2023-05-03 23:08:49 +0200

    removed components since now they are imported from custom library

--- src/components/textTitleStyle.js
+++
@@ -1,8 +0,0 @@
-import { StyleSheet } from "react-native";
-
-export const textTitleStyle = StyleSheet.create({
-    title: {
-        fontFamily: 'Poor Story',
-        color: "#FFAA43",
-    },
-})
