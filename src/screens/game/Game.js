53f2c6aaa8e95dc29488023fcbece53f7cbca97c
Fabrizio Ettori <fabrizio.ettori@beije.it>
2023-05-04 12:36:46 +0200

    added background and controls in game screen

--- src/screens/game/Game.js
+++ src/screens/game/Game.js
@@ -1,15 +1,32 @@
-import React from 'react'
-// Native
-import { View, Text } from 'react-native';
-import { useLocation } from 'react-router-dom';
+// React
+import React from "react";
+// Style
+import { gameStyle } from "./gameStyle";
+// React Native
+import { View } from "react-native";
+// Routing
+import { useLocation } from "react-router-dom";
+// Morra Cinese Components Library
+import { Controls, SpaceBackground } from "morra-cinese-components";
+// Images
+var backgroundImage = require("../../assets/images/game/image-background-1.png");
+var controlsLeft = require("../../assets/images/game/controls_left.png");
+var controlsRight = require("../../assets/images/game/controls_right.png");
 
 function Game() {
     const location = useLocation();
+    console.log("location name", location.state?.name);
+
     return (
-        <View>
-            <Text style={{ color: "white" }}>{location.state?.name}</Text>
-        </View>
-    )
+        <SpaceBackground
+            bgImage={backgroundImage}
+        >
+            <View style={gameStyle.container}>
+                <Controls image={controlsLeft} width={100} height={360} />
+                <Controls image={controlsRight} width={100} height={360} />
+            </View>
+        </SpaceBackground>
+    );
 }
 
-export default Game
+export default Game;
---
+++ src/screens/game/gameStyle.js
@@ -0,0 +1,14 @@
+import { StyleSheet } from "react-native";
+
+export const gameStyle = StyleSheet.create({
+    container: {
+        paddingHorizontal: 4,
+        position: "absolute",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        bottom: 0,
+        height: "75%",
+        width: "100%",
+    },
+})
